9dca6b47fe0fa0e785bfbeb43f71e86e7484348b
Estefania <maria.sassone@mercadolibre.com>
2022-09-09 10:38:05 -0300

    Change Random button and app icons (#11)

--- NotBored/Modules/Activities/View/ActivitiesTableViewController.swift
+++ NotBored/Modules/Activities/View/ActivitiesTableViewController.swift
@@ -7,31 +7,33 @@
 import Foundation
 import UIKit
 
-struct Device {
-    let title: String
-    let imageName: String
-}
-
-let house = [
-    Device(title: "Education", imageName: "chevron.right"),
-    Device(title: "Recreational", imageName: "chevron.right"),
-    Device(title: "Social", imageName: "chevron.right"),
-    Device(title: "Diy", imageName: "chevron.right"),
-    Device(title: "Charity", imageName: "chevron.right"),
-    Device(title: "Cooking", imageName: "chevron.right"),
-    Device(title: "Relaxation", imageName: "chevron.right"),
-    Device(title: "Music", imageName: "chevron.right"),
-    Device(title: "Busywork", imageName: "chevron.right")
+let activitiesList = [
+    Activities(title: "Education", imageName: "chevron.right"),
+    Activities(title: "Recreational", imageName: "chevron.right"),
+    Activities(title: "Social", imageName: "chevron.right"),
+    Activities(title: "Diy", imageName: "chevron.right"),
+    Activities(title: "Charity", imageName: "chevron.right"),
+    Activities(title: "Cooking", imageName: "chevron.right"),
+    Activities(title: "Relaxation", imageName: "chevron.right"),
+    Activities(title: "Music", imageName: "chevron.right"),
+    Activities(title: "Busywork", imageName: "chevron.right")
 ]
 
 class ActivitiesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // MARK: Nav Bar
-    
     override func viewWillLayoutSubviews() {
          
         self.navigationItem.title =  "Activities"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Random", style: .done, target: self, action: #selector(onRightButtonBeenTaped))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem( image: UIImage(systemName: "shuffle"), style: .done, target: self, action: #selector(onRightButtonBeenTaped))
+        self.navigationItem.backButtonTitle = ""
+    }
+    
+    @objc private func onRightButtonBeenTaped(){
+        let service = SuggestionService.shared
+        service.setCategory(category: nil)
+        let sc = SuggestionViewController()
+        self.navigationController?.pushViewController(sc, animated: true)
     }
     
 
@@ -45,7 +47,7 @@ class ActivitiesViewController: UIViewController, UITableViewDataSource, UITable
         super.viewDidLoad()
         
         devicesTableView.backgroundColor = UIColor(red: 218/255, green: 244/255, blue: 254/255, alpha: 1.00)
-       
+        
         devicesTableView.separatorColor = UIColor.black
         devicesTableView.dataSource = self
         devicesTableView.delegate = self
@@ -63,28 +65,19 @@ class ActivitiesViewController: UIViewController, UITableViewDataSource, UITable
         ])
     }
                                         
-                        
-    @objc func selectorX() {}
-    
-    @objc private func onRightButtonBeenTaped(){
-        let service = SuggestionService.shared
-        service.setCategory(category: nil)
-        let sc = SuggestionViewController()
-        self.navigationController?.pushViewController(sc, animated: true)
-    }
     
     // MARK: Metodos TableView
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        house.count
+        activitiesList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ActivitiesTableViewCell", for: indexPath) as! ActivitiesTableViewCell
         
-        let model = house[indexPath.row]
+        let model = activitiesList[indexPath.row]
         cell.configure(model: model)
         
         return cell
@@ -93,7 +86,7 @@ class ActivitiesViewController: UIViewController, UITableViewDataSource, UITable
     // MARK: Metodo Delegate
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let model = house[indexPath.row]
+        let model = activitiesList[indexPath.row]
         print("CELL: \(model.title)")
         
         let service = SuggestionService.shared
